003ab0b15614fd76197b25b07b8489d6983cf165
vanchrisx <christianvannoppen@gmail.com>
2023-06-22 19:16:28 -0300

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./about.css";
+const About = () => {
+  return <div>about</div>;
+};
+
+export default About;
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./contact.css";
+const Contact = () => {
+  return <div>contact</div>;
+};
+
+export default Contact;
